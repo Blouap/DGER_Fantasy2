bc72ae973bcfaea4fb360c5cb13eeea9e6426ab8
Blouap <bugatty@hotmail.fr>
2021-06-01 14:38:00 +0200

    2nd change

---
+++ app/src/main/java/com/example/projet_version1/ChoixPersonnage.java
@@ -0,0 +1,14 @@
+package com.example.projet_version1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoixPersonnage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_personnage);
+    }
+}
